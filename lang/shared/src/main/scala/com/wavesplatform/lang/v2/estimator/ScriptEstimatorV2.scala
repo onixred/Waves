86f2c22b032f0ba4fa72b2f8e0f0f84bb49e050c
Artyom Sayadyan <xrtm000@gmail.com>
2019-08-19 11:58:49 +0300

    Refactor ScriptEstimatorV2

--- lang/shared/src/main/scala/com/wavesplatform/lang/v2/estimator/ScriptEstimatorV2.scala
+++ lang/shared/src/main/scala/com/wavesplatform/lang/v2/estimator/ScriptEstimatorV2.scala
@@ -24,27 +24,34 @@ object ScriptEstimatorV2 {
     local {
       for {
         ctx <- get
-        _   <- modify[EstimatorContext, ExecutionError](userFuncs.modify(_)(funcCtx(_, func, ctx)))
+        _   <- modify[EstimatorContext, ExecutionError](userFuncs.modify(_)(storeFuncCtx(_, func, ctx)))
         r   <- evalExpr(inner)
       } yield r + 5
     }
 
-  private def funcCtx(
-    funcCtx: Map[FunctionHeader, (FUNC, EstimatorContext)],
-    func: FUNC,
-    ctx: EstimatorContext
+  private def storeFuncCtx(
+    funcDef: Map[FunctionHeader, (FUNC, EstimatorContext)],
+    func:    FUNC,
+    ctx:     EstimatorContext
   ): Map[FunctionHeader, (FUNC, EstimatorContext)] =
-      funcCtx.updated(FunctionHeader.User(func.name), (func, lets.set(ctx)(func.args.map((_, (false, const(1)))).toMap)))
+      funcDef.updated(
+        FunctionHeader.User(func.name),
+        (func, lets.set(ctx)(func.args.map((_, (false, const(1)))).toMap))
+      )
 
   private def evalRef(key: String): EvalM[Long] =
     for {
       ctx <- get
       r <- lets.get(ctx).get(key) match {
-        case Some((false, lzy)) => modify[EstimatorContext, ExecutionError](lets.modify(_)(_.updated(key, (true, lzy)))).flatMap(_ => lzy)
+        case Some((false, lzy)) => setRefEvaluated(key, lzy)
         case Some((true,  _))   => const(0)
         case None               => raiseError[EstimatorContext, ExecutionError, Long](s"A definition of '$key' not found")
       }
     } yield r + 2
+
+  private def setRefEvaluated(key: String, lzy: EvalM[Long]): EvalM[Long] =
+    modify[EstimatorContext, ExecutionError](lets.modify(_)(_.updated(key, (true, lzy))))
+      .flatMap(_ => lzy)
 
   private def evalIF(cond: EXPR, ifTrue: EXPR, ifFalse: EXPR): EvalM[Long] =
     for {
@@ -53,32 +60,37 @@ object ScriptEstimatorV2 {
       leftComplexity  <- evalExpr(ifFalse)
     } yield condComplexity + Math.max(leftComplexity, rightComplexity) + 1
 
-  private def evalGetter(expr: EXPR): EvalM[Long] = evalExpr(expr).map(_ + 2)
+  private def evalGetter(expr: EXPR): EvalM[Long] =
+    evalExpr(expr).map(_ + 2)
 
   private def evalFuncCall(header: FunctionHeader, args: List[EXPR]): EvalM[Long] =
     for {
       ctx <- get
-      bodyComplexity <- predefFuncs.get(ctx).get(header).map(const).map(_.flatMap(c => args.traverse(evalExpr).map(_.sum + c)))
-        .orElse(userFuncs.get(ctx).get(header).map(evalLocalFuncCall(_, ctx, args)))
+      bodyComplexity <- predefFuncs.get(ctx).get(header).map(bodyComplexity => evalFuncArgs(args).map(_ + bodyComplexity))
+        .orElse(userFuncs.get(ctx).get(header).map(evalUserFuncCall(_, ctx, args)))
         .getOrElse(raiseError(s"function '$header' not found"))
     } yield bodyComplexity
 
-  private def evalLocalFuncCall(
-      localFuncCtx: (FUNC, EstimatorContext),
-      baseCtx: EstimatorContext,
-      args: List[EXPR]
-  ) = {
+  private def evalUserFuncCall(
+    localFuncCtx: (FUNC, EstimatorContext),
+    baseCtx:      EstimatorContext,
+    args:         List[EXPR]
+  ): EvalM[Long] = {
     val (f, localCtx) = localFuncCtx
     for {
       _ <- modify[EstimatorContext, ExecutionError](userFuncs.set(_)(localCtx.userFuncs))
-      argsComplexity <- args.traverse(evalExpr).map(_.sum)
+      argsComplexity <- evalFuncArgs(args)
       _ <- modify[EstimatorContext, ExecutionError](lets.modify(_)(_ ++ localCtx.letDefs))
-      r <- evalExpr(f.body).map(_ + f.args.size * 5)
+      bodyComplexity <- evalExpr(f.body).map(_ + f.args.size * 5)
       _ <- modify[EstimatorContext, ExecutionError](userFuncs.set(_)(baseCtx.userFuncs))
-    } yield r + argsComplexity
+    } yield bodyComplexity + argsComplexity
   }
 
-  private def const(l: Long): EvalM[Long] = Monad[EvalM].pure(l)
+  private def evalFuncArgs(args: List[EXPR]): EvalM[Long] =
+    args.traverse(evalExpr).map(_.sum)
+
+  private def const(l: Long): EvalM[Long] =
+    Monad[EvalM].pure(l)
 
   private def evalExpr(t: EXPR): EvalM[Long] =
     t match {
